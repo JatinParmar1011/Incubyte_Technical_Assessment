a0a979c213a284bc727248e92b8c1b667412fd62
JatinParmar1011 <jatinparmar1014@gmail.com>
2024-08-31 12:20:33 +0530

    model initialize

---
+++ model/Books.model.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+const BookSchema = new mongoose.Schema({
+    ISBN: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    author: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    publication_year: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: Boolean,
+        default: true 
+    },
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        default: null
+    },
+    person_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Person',
+    }
+},  
+    {
+        timestamps: true
+    }
+);
+
+const Book = mongoose.model('Book', BookSchema);
+
+export default Book;
